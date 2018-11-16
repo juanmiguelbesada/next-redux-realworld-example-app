86255bb19c1485cd7c20b75ae3ce64eeff2c1b76
Juan Miguel Besada <juanmiguelbesada@gmail.com>
2018-11-16 10:38:33 +0100

    chore(): Default page layout

---
+++ components/Header.js
@@ -0,0 +1,26 @@
+export default () => {
+    return (<nav className="navbar navbar-light">
+    <div className="container">
+      <a className="navbar-brand" href="">conduit</a>
+      <ul className="nav navbar-nav pull-xs-right">
+        <li className="nav-item">
+          {/* Add "active" className when you're on that page" */}
+          <a className="nav-link active" href="">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="">
+            <i className="ion-compose"></i>&nbsp;New Post
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="">
+            <i className="ion-gear-a"></i>&nbsp;Settings
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="">Sign up</a>
+        </li>
+      </ul>
+    </div>
+  </nav>)
+}

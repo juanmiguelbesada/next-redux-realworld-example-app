ab386feb0d235cd35e291c8dd8b27857972f8614
Juan Miguel Besada <juanmiguelbesada@gmail.com>
2018-11-16 10:45:41 +0100

    chore(): Generate server routes

--- server.js
+++ server.js
@@ -10,6 +10,30 @@ app.prepare()
   .then(() => {
     const server = express();
 
+    server.get('/login', (req, res) => {
+      return app.render(req, res, '/auth', { action: "login" })
+    })
+
+    server.get('/register', (req, res) => {
+      return app.render(req, res, '/auth', { action: "register" })
+    })
+
+    server.get('/article/:slug', (req, res) => {
+      return app.render(req, res, '/article', { slug: req.params.slug })
+    })
+
+    server.get('/editor/:slug', (req, res) => {
+      return app.render(req, res, '/editor', { slug: req.params.slug })
+    })
+
+    server.get('/profile/:username', (req, res) => {
+      return app.render(req, res, '/profile', { slug: req.params.username })
+    })
+
+    server.get('/profile/:username/favorites', (req, res) => {
+      return app.render(req, res, '/profile', { slug: req.params.username, tab: "favorites" })
+    })
+
     server.get('*', (req, res) => {
       return handle(req, res)
     })
